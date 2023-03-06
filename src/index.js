28604cac0c56691d7fa3819cf75be28a404da9c9
Hasitha Madusanka <hasithamadusankasl@gmail.com>
2023-03-06 15:58:22 +0530

    Aviod duplication using composition (but not inheritance)

--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./index.css"
+import reportWebVitals from "./reportWebVitals"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+//my components
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import HomeGuest from "./components/HomeGuest"
+import About from "./components/About"
+import Terms from "./components/Terms"
 
-//my components 
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeGuest from './components/HomeGuest';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <Header />
-    <HomeGuest />
-    <Footer/>
-  </React.StrictMode>
-);
+    <Routes>
+      <Route path="/" element={<HomeGuest />}></Route>
+      <Route path="/about-us" element={<About />}></Route>
+      <Route path="/terms" element={<Terms />}></Route>
+    </Routes>
+    <Footer />
+  </BrowserRouter>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
