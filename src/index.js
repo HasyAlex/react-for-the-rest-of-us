f9397afc6ca2741ae8705da89b89c396b2d3995d
hasitha madusanka <hasithamadusankasl@.com>
2023-03-06 06:41:44 +0530

    header-homeguest-and-footer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
+//my components 
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeGuest from './components/HomeGuest';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Header />
+    <HomeGuest />
+    <Footer/>
   </React.StrictMode>
 );
 
